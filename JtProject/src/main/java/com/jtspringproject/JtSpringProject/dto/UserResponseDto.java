c05dcb404eea9808bf7588c58b9e1cf245a75e4a
MANISH-Shetty-Repos <manishshettyoth9182@gmail.com>
2026-03-30 10:25:04 +0530

    Feat: DTO - Add UserResponseDto for client communication

---
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/dto/UserResponseDto.java
@@ -0,0 +1,50 @@
+package com.jtspringproject.JtSpringProject.dto;
+
+public class UserResponseDto {
+    private int id;
+    private String username;
+    private String email;
+    private String address;
+    private String role;
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
