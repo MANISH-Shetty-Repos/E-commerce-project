de9dbfd319e49ee1b99ab0c2771be89b5ef80c4b
MANISH-Shetty-Repos <manishshettyoth9182@gmail.com>
2026-03-30 10:22:47 +0530

    Refactor: cartDao - Improvement of cart database operations

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/cartDao.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/cartDao.java
@@ -3,7 +3,8 @@ package com.jtspringproject.JtSpringProject.dao;
 import java.util.List;
 
 import com.jtspringproject.JtSpringProject.models.Cart;
-import com.jtspringproject.JtSpringProject.models.Category;
+import com.jtspringproject.JtSpringProject.models.Product;
+import com.jtspringproject.JtSpringProject.models.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -26,17 +27,32 @@ public class cartDao {
 
     @Transactional
     public List<Cart> getCarts() {
-        return this.sessionFactory.getCurrentSession().createQuery("from CART").list();
+        return this.sessionFactory.getCurrentSession().createQuery("from CART", Cart.class).list();
     }
 
-//    @Transactional
-//    public List<Cart> getCartsByCustomerID(Integer customer_id) {
-//        String hql = "from CART where CART.customer_id = :customer_id";
-//        return this.sessionFactory.getCurrentSession()
-//                .createQuery(hql, Cart.class)
-//                .setParameter("customer_id", customer_id)
-//                .list();
-//    }
+    @Transactional
+    public List<Cart> getCartsByCustomerID(int customer_id) {
+        return this.sessionFactory.getCurrentSession()
+                .createQuery("from CART where customer.id = :customer_id", Cart.class)
+                .setParameter("customer_id", customer_id)
+                .list();
+    }
+
+    @Transactional
+    public List<Product> getProductsInCart(User user) {
+        return this.sessionFactory.getCurrentSession()
+                .createQuery("select cp.product from CartProduct cp where cp.cart.customer = :user", Product.class)
+                .setParameter("user", user)
+                .list();
+    }
+
+    @Transactional
+    public void clearCart(User user) {
+        this.sessionFactory.getCurrentSession()
+                .createQuery("delete from CartProduct cp where cp.cart.customer = :user")
+                .setParameter("user", user)
+                .executeUpdate();
+    }
 
     @Transactional
     public void updateCart(Cart cart) {
