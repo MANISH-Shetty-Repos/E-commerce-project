709657633d4044f1d35ab4ea0386516e4169ae30
MANISH-Shetty-Repos <manishshettyoth9182@gmail.com>
2026-03-30 10:22:47 +0530

    Refactor: cartProductDao - Update cart-product data management

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/cartProductDao.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/cartProductDao.java
@@ -1,6 +1,6 @@
 package com.jtspringproject.JtSpringProject.dao;
 
-import com.jtspringproject.JtSpringProject.models.Cart;
+
 import com.jtspringproject.JtSpringProject.models.CartProduct;
 import com.jtspringproject.JtSpringProject.models.Product;
 import org.hibernate.SessionFactory;
@@ -27,16 +27,19 @@ public class cartProductDao {
 
     @Transactional
     public List<CartProduct> getCartProducts() {
-        return this.sessionFactory.getCurrentSession().createQuery("from CART_PRODUCT ").list();
+        return this.sessionFactory.getCurrentSession().createQuery("from CART_PRODUCT", CartProduct.class).list();
     }
 
     @Transactional
     public List<Product> getProductByCartID(Integer cart_id) {
         String sql = "SELECT product_id FROM cart_product WHERE cart_id = :cart_id";
+        @SuppressWarnings("unchecked")
         List<Integer> productIds = this.sessionFactory.getCurrentSession()
                 .createNativeQuery(sql)
                 .setParameter("cart_id", cart_id)
                 .list();
+
+        if (productIds.isEmpty()) return new java.util.ArrayList<>();
 
         sql = "SELECT * FROM product WHERE id IN (:product_ids)";
         return this.sessionFactory.getCurrentSession()
