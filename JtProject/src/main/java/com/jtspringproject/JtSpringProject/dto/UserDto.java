839d3295916f9fb4b5f6bfd5e6a2d288f3080178
MANISH-Shetty-Repos <manishshettyoth9182@gmail.com>
2026-03-30 10:25:04 +0530

    Feat: DTO - Add UserDto for data transfer

---
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/dto/UserDto.java
@@ -0,0 +1,56 @@
+package com.jtspringproject.JtSpringProject.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserDto {
+    
+    @NotBlank(message = "Username cannot be empty")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    @NotBlank(message = "Address cannot be empty")
+    private String address;
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
